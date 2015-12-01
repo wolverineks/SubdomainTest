48c554e9d0118d1f18b196d6fd53630a6fee001f
wolverine.ks <wolverine.ks@gmail.com>
2015-12-01 10:17:40 -0800

    up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
 
-  root to: 'blogs#index'
-
-  get 'posts/show'
-
-  get 'blogs/index'
-
-  get 'blogs/show'
+  resources :posts
+  resources :blogs
 
   match '/', to: 'blogs#index', constraints: { subdomain: 'www' }, via: [:get, :post, :put, :patch, :delete]
-  
   match '/', to: 'blogs#show', constraints: { subdomain: /.+/ }, via: [:get, :post, :put, :patch, :delete]
+  
+  root to: 'blogs#index'
 
 end
